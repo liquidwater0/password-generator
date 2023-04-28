b8edf672ee6c1f7e4bff5e55d3e11f2aff430a57
liquidwater0 <32186774+liquidwater0@users.noreply.github.com>
2023-04-28 14:57:33 -0500

    put these array functions in own file

--- src/context/PasswordContext.tsx
+++ src/context/PasswordContext.tsx
@@ -8,6 +8,7 @@ import {
     Dispatch,
 	Reducer
 } from "react";
+import { getUppercaseLetters, getLowercaseLetters, getNumbers, getSymbols } from "../utitities";
 
 type State = {
 	password: string,
@@ -30,65 +31,6 @@ const PasswordContext = createContext<PasswordContextType>(null!);
 
 export function usePassword() {
     return useContext(PasswordContext);
-}
-
-function getUppercaseLetters() {
-	let uppercaseLetters: string[] = [];
-
-	for (let i = 65; i <= 90; i++) {
-		const letter = String.fromCharCode(i);
-		uppercaseLetters.push(letter);
-	}
-
-	return uppercaseLetters;
-}
-
-function getLowercaseLetters() {
-	let lowercaseLetters: string[] = [];
-
-	for (let i = 97; i <= 122; i++) {
-		const letter = String.fromCharCode(i);
-		lowercaseLetters.push(letter);
-	}
-
-	return lowercaseLetters;
-}
-
-function getNumbers() {
-	let numbers: string[] = [];
-
-	for (let i = 48; i <= 57; i++) {
-		const letter = String.fromCharCode(i);
-		numbers.push(letter);
-	}
-
-	return numbers;
-}
-
-function getSymbols() {
-	let symbols: string[] = [];
-
-	for (let i = 33; i <= 47; i++) {
-		const letter = String.fromCharCode(i);
-		symbols.push(letter);
-	}
-
-	for (let i = 58; i <= 64; i++) {
-		const letter = String.fromCharCode(i);
-		symbols.push(letter);
-	}
-
-	for (let i = 91; i <= 96; i++) {
-		const letter = String.fromCharCode(i);
-		symbols.push(letter);
-	}
-
-	for (let i = 123; i <= 126; i++) {
-		const letter = String.fromCharCode(i);
-		symbols.push(letter);
-	}
-
-	return symbols;
 }
 
 export const MIN_LENGTH = 5;
