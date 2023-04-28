ac8d931ba42ae2d8d64585292dd6655b7434c23a
liquidwater0 <32186774+liquidwater0@users.noreply.github.com>
2023-04-27 19:43:16 -0500

    add functions

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,65 @@ import { useState } from 'react';
 import "./scss/App.scss";
 import { Autorenew, ContentCopy } from '@mui/icons-material';
 
+function getUppercaseLetters() {
+	let uppercaseLetters: string[] = [];
+
+	for (let i = 65; i <= 90; i++) {
+		const letter = String.fromCharCode(i);
+		uppercaseLetters.push(letter);
+	}
+
+	return uppercaseLetters;
+}
+
+function getLowercaseLetters() {
+	let lowercaseLetters: string[] = [];
+
+	for (let i = 97; i <= 122; i++) {
+		const letter = String.fromCharCode(i);
+		lowercaseLetters.push(letter);
+	}
+
+	return lowercaseLetters;
+}
+
+function getNumbers() {
+	let numbers: string[] = [];
+
+	for (let i = 48; i <= 57; i++) {
+		const letter = String.fromCharCode(i);
+		numbers.push(letter);
+	}
+
+	return numbers;
+}
+
+function getSymbols() {
+	let symbols: string[] = [];
+
+	for (let i = 33; i <= 47; i++) {
+		const letter = String.fromCharCode(i);
+		symbols.push(letter);
+	}
+
+	for (let i = 58; i <= 64; i++) {
+		const letter = String.fromCharCode(i);
+		symbols.push(letter);
+	}
+
+	for (let i = 91; i <= 96; i++) {
+		const letter = String.fromCharCode(i);
+		symbols.push(letter);
+	}
+
+	for (let i = 123; i <= 126; i++) {
+		const letter = String.fromCharCode(i);
+		symbols.push(letter);
+	}
+
+	return symbols;
+}
+
 function App() {
 	const [passwordLength, setPasswordLength] = useState<number>(10);
 	const [includeUppercase, setIncludeUppercase] = useState<boolean>(true);
