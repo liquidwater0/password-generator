dc5b53504857aa9b0741a662a2c06aca4ee89775
liquidwater0 <32186774+liquidwater0@users.noreply.github.com>
2023-04-29 13:53:35 -0500

    remove old code

--- src/components/NumberInput.tsx
+++ src/components/NumberInput.tsx
@@ -43,24 +43,3 @@ export default function NumberInput({ min, max, value, defaultValue, ...props }:
         </div>
     );
 }
-
-// import { HTMLAttributes } from 'react';
-
-// type NumberInputProps = {
-//     min?: number,
-//     max?: number,
-//     value?: number
-// } & HTMLAttributes<HTMLInputElement>;
-
-// export default function NumberInput({ min, max, value, ...props }: NumberInputProps) {
-//     return (
-//         <input 
-//             className='number-input'
-//             type="number" 
-//             min={min}
-//             max={max}
-//             value={value}
-//             { ...props }
-//         />
-//     );
-// }
